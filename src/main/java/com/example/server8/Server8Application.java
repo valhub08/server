33fe7cb490f65ed85ee7230caf9ada64ca74d984
valhub08 <jhstl1203@gmail.com>
2024-09-20 09:42:42 +0900

    first

---
+++ src/main/java/com/example/server8/Server8Application.java
@@ -0,0 +1,13 @@
+package com.example.server8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server8Application.class, args);
+    }
+
+}
