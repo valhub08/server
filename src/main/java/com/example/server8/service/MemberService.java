753ff7e78a657551f172a40cdb16e9fae67df106
valhub08 <jhstl1203@gmail.com>
2024-09-21 12:58:30 +0900

    MemberService

---
+++ src/main/java/com/example/server8/service/MemberService.java
@@ -0,0 +1,50 @@
+package com.example.server8.service;
+
+import com.example.server8.dto.JoinRequest;
+import com.example.server8.dto.LoginRequest;
+import com.example.server8.member.Member;
+import com.example.server8.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public boolean checkLoginIdDuplicate(String loginId){
+        return memberRepository.existsByLoginId(loginId);
+    }
+
+
+    public void join(JoinRequest joinRequest) {
+        memberRepository.save(joinRequest.toEntity());
+    }
+
+    public Member login(LoginRequest loginRequest) {
+        Member findMember = memberRepository.findByLoginId(loginRequest.getLoginId());
+
+        if(findMember == null){
+            return null;
+        }
+
+        if (!findMember.getPassword().equals(loginRequest.getPassword())) {
+            return null;
+        }
+
+        return findMember;
+    }
+
+    public Member getLoginMemberById(Long memberId){
+        if(memberId == null) return null;
+
+        Optional<Member> findMember = memberRepository.findById(memberId);
+        return findMember.orElse(null);
+
+    }
+}
