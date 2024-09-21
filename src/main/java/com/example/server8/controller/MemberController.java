4bb8371ceaec07b5fdd3e720f8b735fbd03d917a
valhub08 <jhstl1203@gmail.com>
2024-09-21 16:36:41 +0900

    MemberController

---
+++ src/main/java/com/example/server8/controller/MemberController.java
@@ -0,0 +1,33 @@
+package com.example.server8.controller;
+
+
+import com.example.server8.dto.MemberDTO;
+import com.example.server8.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor //MemberService에 대한 멤버를 사용 가능
+public class MemberController {
+
+    // 생성자 주입
+    private final MemberService memberService;
+
+    // 회원가입 페이지 출력 요청
+    @GetMapping("/member/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/member/save")    // name값을 requestparam에 담아온다
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        System.out.println("MemberController.save");
+        System.out.println("memberDTO = " + memberDTO);
+        memberService.save(memberDTO);
+
+        return "index";
+    }
+}
